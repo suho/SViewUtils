23b0bc8a59d18a2a95a26a1e27fd2fbec11fb27c
hovansu <hovansu.it@gmail.com>
2017-05-24 11:56:39 +0700

    no message

---
+++ SViewUtils/SViewUtils.h
@@ -0,0 +1,19 @@
+//
+//  SViewUtils.h
+//  SViewUtils
+//
+//  Created by Mylo Ho on 5/24/17.
+//  Copyright © 2017 SuHoVan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SViewUtils.
+FOUNDATION_EXPORT double SViewUtilsVersionNumber;
+
+//! Project version string for SViewUtils.
+FOUNDATION_EXPORT const unsigned char SViewUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SViewUtils/PublicHeader.h>
+
+
